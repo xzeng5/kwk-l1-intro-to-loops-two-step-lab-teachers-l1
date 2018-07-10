b664409894988a25e1a331190f16670cb8fcbee2
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-10 15:05:36 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -12,63 +12,63 @@ def first_steps
     sleep(1)
   end
 end
-
-# Write a loop that outputs the first two sets of steps in the Two-Step
-def a_few_more_steps
-  loop do
-    puts "Right foot back"
-    sleep(0.5)
-    puts "Left foot back"
-    sleep(0.5)
-    puts "Right foot back"
-    sleep(0.5)
-    puts "Stop"
-    sleep(1)
-    puts "Right foot steps right and back"
-    sleep(0.5)
-    puts "Left foot crosses over right"
-    sleep(0.5)
-    puts "Right foot steps right"
-    sleep(0.5)
-    puts "Turn"
-    sleep(1)
-  end
-end
-
-# Write a loop that fulfills all criteria from Part 3 of the README.md
-def how_many_steps?
-  steps = 0
-
-  loop do
-    steps += 1
-    if steps % 2 == 0
-      puts "#{steps}"
-      puts "Left"
-    else
-      puts "#{steps}"
-      puts "Right"
-    end
-    sleep(0.5)
-  end
-end
-
-# Write a solution that uses the same code as how_many_steps?, but breaks the
-# loop if steps is equal to 6
-def break_dance
-  steps = 0
-
-  loop do
-    steps += 1
-    if steps % 2 == 0
-      puts "#{steps}"
-      puts "Left"
-    else
-      puts "#{steps}"
-      puts "Right"
-    end
-    sleep(0.5)
-    if steps == 6
-      break
-    end
-  end
-end
+# 
+# # Write a loop that outputs the first two sets of steps in the Two-Step
+# def a_few_more_steps
+#   loop do
+#     puts "Right foot back"
+#     sleep(0.5)
+#     puts "Left foot back"
+#     sleep(0.5)
+#     puts "Right foot back"
+#     sleep(0.5)
+#     puts "Stop"
+#     sleep(1)
+#     puts "Right foot steps right and back"
+#     sleep(0.5)
+#     puts "Left foot crosses over right"
+#     sleep(0.5)
+#     puts "Right foot steps right"
+#     sleep(0.5)
+#     puts "Turn"
+#     sleep(1)
+#   end
+# end
+# 
+# # Write a loop that fulfills all criteria from Part 3 of the README.md
+# def how_many_steps?
+#   steps = 0
+# 
+#   loop do
+#     steps += 1
+#     if steps % 2 == 0
+#       puts "#{steps}"
+#       puts "Left"
+#     else
+#       puts "#{steps}"
+#       puts "Right"
+#     end
+#     sleep(0.5)
+#   end
+# end
+# 
+# # Write a solution that uses the same code as how_many_steps?, but breaks the
+# # loop if steps is equal to 6
+# def break_dance
+#   steps = 0
+# 
+#   loop do
+#     steps += 1
+#     if steps % 2 == 0
+#       puts "#{steps}"
+#       puts "Left"
+#     else
+#       puts "#{steps}"
+#       puts "Right"
+#     end
+#     sleep(0.5)
+#     if steps == 6
+#       break
+#     end
+#   end
+# end
